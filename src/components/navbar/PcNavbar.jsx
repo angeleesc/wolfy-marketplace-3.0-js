72dc7e0310cd0784269ff46da9385f4ab1e73171
unknown <angeleesc2@gmail.com>
2023-01-30 19:18:08 -0400

    confuguracion de la zona

--- src/components/navbar/PcNavbar.jsx
+++ src/components/navbar/PcNavbar.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from "react";
+import Logo from "../icons/Logo";
 
 export default function PcNavbar() {
   return (
-    <div className='wolf-pc-navbar' >PcNavbar</div>
-  )
+    <div className="wolf-pc-navbar">
+      <div className="logo">
+        <Logo />
+        <h3 className="ml-2 text-wolf-gray-950">Wolf Inu</h3>
+      </div>
+      <div className="main-option" >
+        
+      </div>
+    </div>
+  );
 }
