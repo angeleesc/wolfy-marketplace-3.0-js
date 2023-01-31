efccf42e53da677a39d75fa66cb8894085f4d5f9
unknown <angeleesc2@gmail.com>
2023-01-30 20:59:25 -0400

    conexion de redux con react

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "./scss/index.scss"
 import { RouterProvider } from 'react-router-dom';
 import router from './routes/Routes';
+import { Provider } from 'react-redux';
+import store from './redux-app/stores';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <Provider store={store} >
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
