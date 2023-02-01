9109495d719443f12398044186862ba6c350c0e2
unknown <angeleesc2@gmail.com>
2023-02-01 14:24:00 -0400

    creacion de wolfy tooltips

---
+++ src/components/tooltip/WolfTooltip.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { styled } from "@mui/material/styles"
+import { Tooltip } from '@mui/material'
+
+const WolfTooltip = styled(({ className, ...props }) => (
+    <Tooltip {...props} classes={{ popper: className }} />
+  ))(({ theme }) => ({
+    [`& .${tooltipClasses.tooltip}`]: {
+      backgroundColor: theme.palette.common.white,
+      color: 'rgba(0, 0, 0, 0.87)',
+      boxShadow: theme.shadows[1],
+      fontSize: 11,
+    },
+  }));
+
+  export default WolfTooltip
