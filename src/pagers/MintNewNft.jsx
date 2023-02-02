018975884bee8562f0ecb1b37eaf7899c21f6f04
unknown <angeleesc2@gmail.com>
2023-02-01 21:25:37 -0400

    configuracion de la zona

--- src/pagers/MintNewNft.jsx
+++ src/pagers/MintNewNft.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from "react";
 
 export default function MintNewNft() {
   return (
-    <div className='page-box' >MintNewNft Xd</div>
-  )
+    <div className="page-box">
+      <section className="page-body">
+        <h3>Page</h3>
+        <div className="grid grid-cols-1 md:grid-cols-2">
+          <div>
+            <h3>Fomulario</h3>
+          </div>
+          <div>
+            <h3>previsualizacio</h3>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
