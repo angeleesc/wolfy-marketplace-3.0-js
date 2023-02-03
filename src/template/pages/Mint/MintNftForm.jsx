6361bdebfc3f3925f3a11c270b2dab3128c1dbd8
unknown <angeleesc2@gmail.com>
2023-02-03 19:36:09 -0400

    configguracion del check add

--- src/template/pages/Mint/MintNftForm.jsx
+++ src/template/pages/Mint/MintNftForm.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import WTextFields from "../../../components/form-controls/inputs/WTextFields";
 import WTextAreaInput from "../../../components/form-controls/inputs/WTextAreaInput";
+import { FormControlLabel, Switch } from "@mui/material";
 
 export default function MintNftForm() {
+
+  const [isAddAtribute, setIsAddAtribute] = useState(false)
+  
   return (
     <form className="wolf-form">
       <h3 className="my-4">Datos Basico de la nft</h3>
@@ -28,11 +32,18 @@ export default function MintNftForm() {
       <WTextFields
         id="nft-url"
         textLabel="Enlace de la url"
-        info="Puedes colocar el link de la pagina url donde esta alojado principalmente la nft este campo es opcional. nota te recomendamos que utilises una url ipfs para que tu oferta sea para siempre"
-        // errorMessage="este campo es requerido"
-        // warningMessage="advertencia este nomber no es original"
-        placeholder="Gato fans 151"
+        info="Puedes colocar el link de la pagina url donde esta alojado principalmente la nft este campo es opcional. nota te recomendamos que utilises una url ipfs para que tu pagina sea para siempre"
+        placeholder="Ej https://www.gatos-crypto.com"
       />
+
+      <FormControlLabel
+       label="Agregar los atribhutos a esta nft" 
+       control={
+        <Switch   name="isAddPropieties"  checked={isAddAtribute} onChange={e=>{
+          setIsAddAtribute(e.target.checked)
+        }} />
+       }
+       />
     </form>
   );
 }
