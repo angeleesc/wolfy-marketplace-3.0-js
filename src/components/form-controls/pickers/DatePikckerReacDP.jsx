919803ba9486adfb15ee76b7fcbf658a042d63d0
unknown <angeleesc2@gmail.com>
2023-02-07 15:38:02 -0400

    configuracion del range date

--- src/components/form-controls/pickers/DatePikckerReacDP.jsx
+++ src/components/form-controls/pickers/DatePikckerReacDP.jsx
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
+import addTime from "date-fns/add";
 import DatePicker from "react-datepicker";
 import "./c-date-picker.scss";
+import "./date-picker.scss";
 
-
-export default function DatePikckerReacDP() {
-  const [startDate, setStartDate] = useState(new Date());
-  const [endDate, setEndDate] = useState(null);
+export default function DatePikckerReacDP({ defaultTime = new Date() }) {
+  const [startDate, setStartDate] = useState(defaultTime);
+  const [endDate, setEndDate] = useState(
+    addTime(defaultTime, {
+      days: 1,
+    })
+  );
   const onChange = (dates) => {
     const [start, end] = dates;
     setStartDate(start);
@@ -14,13 +19,21 @@ export default function DatePikckerReacDP() {
 
   return (
     <div>
+      <div className="calendar-box">
         <DatePicker
           selected={startDate}
-          onChange={(date) => setStartDate(date)}
+          onChange={onChange}
+          startDate={startDate}
+          endDate={endDate}
+          selectsRange
+          selectsDisabledDaysInRange
           inline
           monthsShown={2}
-          
         />
+        <div>
+          <input type="time" />
+        </div>
+      </div>
     </div>
   );
 }
