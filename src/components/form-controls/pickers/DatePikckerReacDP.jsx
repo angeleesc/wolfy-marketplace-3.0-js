1235f775a0313e72bb7e885c5c7953ed6806fd0d
unknown <angeleesc2@gmail.com>
2023-02-07 14:33:22 -0400

    change date

--- src/components/form-controls/pickers/DatePikckerReacDP.jsx
+++ src/components/form-controls/pickers/DatePikckerReacDP.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "./c-date-picker.scss";
+
 
 export default function DatePikckerReacDP() {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(null);
+  const onChange = (dates) => {
+    const [start, end] = dates;
+    setStartDate(start);
+    setEndDate(end);
+  };
+
   return (
-    <div>DatePikckerReacDP</div>
-  )
+    <div>
+        <DatePicker
+          selected={startDate}
+          onChange={(date) => setStartDate(date)}
+          inline
+          monthsShown={2}
+          
+        />
+    </div>
+  );
 }
