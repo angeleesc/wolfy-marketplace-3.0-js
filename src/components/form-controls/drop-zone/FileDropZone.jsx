2f7ec0be3035fdb7bb5c953716e121fae2cf5ba9
unknown <angeleesc2@gmail.com>
2023-02-08 12:16:02 -0400

    configuracion gacica del file drop zone

--- src/components/form-controls/drop-zone/FileDropZone.jsx
+++ src/components/form-controls/drop-zone/FileDropZone.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useCallback } from "react";
+import "./file-drop-zone.scss"
+import { useDropzone } from "react-dropzone";
 
 export default function FileDropZone() {
+
+const onDrop = useCallback((files)=>{
+  console.log(files)
+},[])
+
+
+  const  { getRootProps, getInputProps } = useDropzone({onDrop})
+
   return (
-    <div className='file-drop-zone' >
-        
+    <div className="wolf-file-drop-zone">
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      </div>
     </div>
-  )
+  );
 }
