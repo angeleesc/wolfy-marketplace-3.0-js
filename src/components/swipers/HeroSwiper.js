0c2289d20bf68be8e9494463c4b97447a220059e
unknown <angeleesc2@gmail.com>
2023-02-13 14:31:25 -0400

    configuracion base de estilo del swiper

--- src/components/swipers/HeroSwiper.js
+++ src/components/swipers/HeroSwiper.js
@@ -68,7 +68,6 @@ const tempJson = [
 export default () => {
   return (
     <div className="wolfy-hero">
-      <h4>Swiper</h4>
       <Swiper
         // install Swiper modules
         spaceBetween={50}
@@ -79,10 +78,18 @@ export default () => {
         onSwiper={(swiper) => console.log(swiper)}
         onSlideChange={() => console.log("slide change")}
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
+        {
+          tempJson.map((collitem, i) => {
+            return (
+              <SwiperSlide className="collection-container">
+                <div className="collection-item" >
+                  <img className="cover-img" src={collitem.coverImg} alt={"hero-img-" + i} />
+                </div>
+              </SwiperSlide>
+            )
+          })
+        }
+
       </Swiper>
     </div>
   );
