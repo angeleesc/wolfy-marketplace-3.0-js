7e373b92f3da222c80041509d237a3db5d24489a
unknown <angeleesc2@gmail.com>
2023-02-24 21:43:35 -0400

    cofiguracion de la zona

--- src/template/modals/checkoutModal/CheckoutModal.jsx
+++ src/template/modals/checkoutModal/CheckoutModal.jsx
@@ -12,6 +12,7 @@ import { useForm, Controller } from "react-hook-form";
 import { closeModal } from "../../../features/modals/modalsSlice";
 import "./checkout-modal.scss";
 import { Skeleton } from "@mui/material";
+import WolfSad from "../../../components/icons/WolfSad";
 
 export default function CheckoutModal() {
   const [stepProcces, setStepProcces] = useState(2);
@@ -65,37 +66,43 @@ export default function CheckoutModal() {
           <div className="wolf-modal-body w-[100%] max-w-[560px] min-h-[40px]">
             <h2 className="title mb-[10px]">Checkout</h2>
 
-            <div className="thumbnails-nfts">
-              <div className="multimedia-zone">
-                <img src={modalData.nftCover} alt="checkout-nft-cover" />
-              </div>
-              <div className="price-zone">
-                <div className="price-zone-item">
-                  <span className="key">Nombre :</span>
-                  <span className="value">La nft xd</span>
+            {stepProcces <= 0 && (
+              <div className="thumbnails-nfts">
+                <div className="multimedia-zone">
+                  <img src={modalData.nftCover} alt="checkout-nft-cover" />
                 </div>
-                <div className="price-zone-item">
-                  <span className="key">Disponible :</span>
-                  <span className="value">1</span>
+                <div className="price-zone">
+                  <div className="price-zone-item">
+                    <span className="key">Nombre :</span>
+                    <span className="value">La nft xd</span>
+                  </div>
+                  <div className="price-zone-item">
+                    <span className="key">Disponible :</span>
+                    <span className="value">1</span>
+                  </div>
                 </div>
               </div>
-            </div>
+            )}
 
-            <div className="wallet-section">
-              <div></div>
-              <div className="flex items-center">
-                <div className="wolf-blockchain-logo">
-                  <div className="uncenected-signer"></div>
-                  <BlockChainIcon />
-                  <span className=""></span>
+            {stepProcces <= 0 && (
+              <div className="wallet-section">
+                <div></div>
+                <div className="flex items-center">
+                  <div className="wolf-blockchain-logo">
+                    <div className="uncenected-signer"></div>
+                    <BlockChainIcon />
+                    <span className=""></span>
+                  </div>
+                  <div className="ml-[15px] wallet-accoutn flex flex-col">
+                    <span className="wallet-public-key">
+                      0x23b05735...4d64f
+                    </span>
+                    <span className="blochain-name">Polygon</span>
+                  </div>
                 </div>
-                <div className="ml-[15px] wallet-accoutn flex flex-col">
-                  <span className="wallet-public-key">0x23b05735...4d64f</span>
-                  <span className="blochain-name">Polygon</span>
-                </div>
+                <div className="w-100% h-[1px] bg-wolf-gray-dark-1000 my-[10px]"></div>
               </div>
-              <div className="w-100% h-[1px] bg-wolf-gray-dark-1000 my-[10px]"></div>
-            </div>
+            )}
             {stepProcces === 0 && (
               <form
                 className="bill-checkout-zone px-[15px]"
@@ -299,8 +306,36 @@ export default function CheckoutModal() {
             )}
 
             {stepProcces === 2 && (
-              <div className="p-2 fail-process">
-                <h3>Ocurrio un Error</h3>
+              <div className="px-[15px] flex flex-col justify-center items-center  fail-process">
+                <h3 className="my-3">Ocurrio un Error</h3>
+                <WolfSad />
+                <span className="text-[14px] text-wolf-gray-light-800">
+                  Ocurrio u error durante la transsacion
+                </span>
+                <div className="w-[100%] mt-[10px]">
+                  <button
+                    type="button"
+                    className="wolf-buttom wolf-buttom-primary w-[100%]"
+                  >
+                    Intentar nuevamente
+                  </button>
+                  <div className="w-[100%] h-[1px] bg-wolf-gray-dark-1200 my-[15px]"  ></div>
+                  <div className="mt-[10px]">
+                    <button
+                      type="button"
+                      className="wolf-buttom w-[100%]  hover:bg-wolf-blue-200"
+                      onClick={() => {
+                        dispatch(
+                          closeModal({
+                            modal: keyModalSate.checkoutModal,
+                          })
+                        );
+                      }}
+                    >
+                      Cancelar
+                    </button>
+                  </div>
+                </div>
               </div>
             )}
           </div>
