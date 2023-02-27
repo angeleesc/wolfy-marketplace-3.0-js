4364fd0dcfa59a62f0a9045d9be1154db8e414f9
unknown <angeleesc2@gmail.com>
2023-02-27 18:52:33 -0400

    place config

--- src/components/cards/CollectionCard.jsx
+++ src/components/cards/CollectionCard.jsx
@@ -20,8 +20,16 @@ export default function CollectionCard({ collectionData }) {
     <div className="collection-card">
       <div className="collection-card-body">
         <div className="multimedia-data">
-          <div className="img-zone-1 mr-2">
+          <div className="img-zone-1 mr-2 relative">
             {sampleImgs[0] && <img src={sampleImgs[0]} alt="sample-img-1" />}
+            <div className="avatar-zone">
+              <div className="avatar-item mb-2">
+                <img src={authorAvartar} alt="authro-avatar" />
+              </div>
+              <div className="avatar-item">
+                <img src={coverImg} alt="cover-collection" />
+              </div>
+            </div>
           </div>
           <div className="img-zone-2">
             <div className="img-zone-21">
@@ -41,6 +49,9 @@ export default function CollectionCard({ collectionData }) {
               {sampleImgs[3] && <img src={sampleImgs[3]} alt="img-sample-4" />}
             </div>
           </div>
+        </div>
+        <div className="info-data">
+          <h4 className="colection-tite">{collectionName}</h4>
         </div>
       </div>
     </div>
