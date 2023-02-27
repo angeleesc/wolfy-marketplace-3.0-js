45b84701a24312cf798e5e63c13a54a457340560
unknown <angeleesc2@gmail.com>
2023-02-27 18:00:33 -0400

    maquetado de la imagenes de la collecion

--- src/components/cards/CollectionCard.jsx
+++ src/components/cards/CollectionCard.jsx
@@ -1,12 +1,48 @@
-import React from 'react'
+import React from "react";
+import "./collection-card.scss";
 
-export default function CollectionCard({collectionData}) {
-
-
+export default function CollectionCard({ collectionData }) {
+  const {
+    collectionName,
+    collectionAuthor,
+    item,
+    floorPrice,
+    onSale,
+    owners,
+    isColectionVerified,
+    isAuthorVerified,
+    authorAvartar,
+    coverImg,
+    sampleImgs,
+  } = collectionData;
 
   return (
-    <div className='collection-card' >
-        <h3>card</h3>
+    <div className="collection-card">
+      <div className="collection-card-body">
+        <div className="multimedia-data">
+          <div className="img-zone-1 mr-2">
+            {sampleImgs[0] && <img src={sampleImgs[0]} alt="sample-img-1" />}
+          </div>
+          <div className="img-zone-2">
+            <div className="img-zone-21">
+              <div className="img-zone-211 mr-1">
+                {sampleImgs[1] && (
+                  <img src={sampleImgs[1]} alt="sample-img-2" />
+                )}
+              </div>
+              <div className="img-zone-212 ml-1">
+                {sampleImgs[2] && (
+                  <img src={sampleImgs[2]} alt="sample-img-3" />
+                )}
+              </div>
+            </div>
+
+            <div className="img-zone-22 mt-2">
+              {sampleImgs[3] && <img src={sampleImgs[3]} alt="img-sample-4" />}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
