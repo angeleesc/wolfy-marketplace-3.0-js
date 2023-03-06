cef32e8802d18f781b81aff44fa108a7cff77e3b
unknown <angeleesc2@gmail.com>
2023-03-06 19:51:59 -0400

    fle xol

--- src/components/price-zone/PriceZoen.jsx
+++ src/components/price-zone/PriceZoen.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import "./price-zone.scss";
+import { saleMethod as saleMethosOption } from "../../helpers/global-constants";
+import WolfIdentidcon from "../identicon/WolfIdentidcon";
 
 export default function PriceZoen({
   price,
@@ -7,9 +10,37 @@ export default function PriceZoen({
   seller,
   sellerName,
   sellerAvatar,
-  expirationTime
+  expirationTime,
 }) {
-  return <div className="sale-info-price" >
-    <h3>saleimgo Xd</h3>
-  </div>;
+  return (
+    <div className="sale-info-price">
+      <div className="flex justify-between">
+        {saleMethod === saleMethosOption.sales && (
+          <div className="flex w-[100%] justify-between items-center flex-col-reverse min-[600px]:flex-row ">
+            <div className="flex items-start my-3 ">
+              <WolfIdentidcon size={40} />
+              <div className="h-[100%] ml-2 flex flex-col justify-center">
+                <span className="text-[12px] text-wolf-gray-light-1000">
+                  Vendedor
+                </span>
+                <h3 className="text-[15px] text-wolf-gray-light-1500 leading-[15px] font-semibold ">
+                  {sellerName
+                    ? sellerName
+                    : `${seller.substring(0, 5)}...${seller.substr(-5)}`}
+                </h3>
+              </div>
+            </div>
+            <div className="flex flex-col my-3">
+              <span className="text-[12px] text-wolf-gray-light-1000">
+                En venta
+              </span>
+              <h3 className="text-[26px] leading-[28px] text-wolf-gray-light-1900 font-semibold" >
+                {price} ETH
+              </h3>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
