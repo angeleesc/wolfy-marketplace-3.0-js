7a12c2e600e800c6ed6630e0cfff4b42a515db66
unknown <angeleesc2@gmail.com>
2023-03-10 18:06:15 -0400

    curren ref

--- src/components/navbar/PcNavbar.jsx
+++ src/components/navbar/PcNavbar.jsx
@@ -1,31 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useMemo, useRef } from "react";
 import { useSelector } from "react-redux";
 import Logo from "../icons/Logo";
+import { Link, matchRoutes, useLocation } from "react-router-dom";
+import { staticRoutes } from "../../helpers/static-routes";
 
 export default function PcNavbar() {
-  const [openEplorerDpopOption, setopenEplorerDpopOption] = useState(false);
-  const [createNftDrop, setCreateNftDrop] = useState(false);
+  const routes = useLocation();
+  const bodyRef = useRef();
 
-  const uiNavbarInfo = useSelector((state) => state.language.navBar);
-  const {
-    exporeOptionTextUi,
-    createOptionUi,
-    resurseOptionUi,
-    dropMenuSection,
-  } = uiNavbarInfo;
-  // console.log(dropMenuSection.exporeOptionTextUi.dorpMenuOptions.NftsTextUi);
-
-  // console.log(uiNavbarInfo);
+  useEffect(() => {
+    if (bodyRef.current) {
+      console.log(bodyRef.current.classList);
+    }
+  }, []);
 
   return (
     <div className="wolf-pc-navbar">
-      <div className="wolf-pc-navbar-body">
-        <div className="flex items-center bg-slate-500 relative h-[100%]">
+      <div className="wolf-pc-navbar-body mode-explorer" ref={bodyRef}>
+        <Link
+          className="logo-section flex items-center  relative h-[60px]"
+          to="/"
+        >
           <div className="logo-img">
             <Logo size="60" />
           </div>
-          <h1 className="ml-[70px]">Wolf Inu</h1>
-        </div>
+          <h1 className="ml-[85px]">Wolf Inu</h1>
+        </Link>
       </div>
     </div>
   );
