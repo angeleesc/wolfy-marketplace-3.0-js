401ea4b7de3bc9bb719f01c3691cb82a5027a74d
unknown <angeleesc2@gmail.com>
2023-03-13 18:09:32 -0400

    eliminacion de componentes inutiles

--- src/components/fillter/SortByFillter.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function SortByFillter() {
-  return (
-    <div>
-        <h3>sor by</h3>
-    </div>
-  )
-}
--- src/components/menus/ExplorerMenuFillter.jsx
+++ src/components/menus/ExplorerMenuFillter.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import "./explorer-menus.scss";
-import BlockchainFillter from "../fillter/BlockchainFillter";
-import SaleMethodFillter from "../fillter/SaleMethodFillter";
 import RangePriceFillter from "../fillter/RangePriceFillter";
-import SortByFillter from "../fillter/SortByFillter";
 import DropMenuOptionFillter from "../fillter/DropMenuOption";
 import {
   blockChainLisoption,
