14081d7b0824da76958a87f5fe9c95babc86d3d1
unknown <angeleesc2@gmail.com>
2023-03-13 15:49:52 -0400

    white space no warp

--- src/components/fillter/DropMenuOption.jsx
+++ src/components/fillter/DropMenuOption.jsx
@@ -10,6 +10,7 @@ export default function DropMenuOptionFillter({
 }) {
   const [openDrop, setOpenDrop] = useState(false);
   const [currentValue, setCurrentValue] = useState("");
+  const [valueUi, setValueUi] = useState("");
   const activatorLisRef = useRef();
   const dropDownlistRef = useRef();
 
@@ -42,16 +43,26 @@ export default function DropMenuOptionFillter({
         <div className="drop-menu-item-xd" ref={dropDownlistRef}>
           <ul>
             {optioms.map((options, i) => {
+              const { icon: IconRenderOp } = options;
+
               return (
                 <li
                   key={"blockhcainoption-" + i}
                   onClick={() => {
                     console.log("cliccleado", options.name);
-                    setCurrentValue(options.name);
+                    setCurrentValue(options.value);
+                    setValueUi(options.name);
                     setOpenDrop(false);
                   }}
                 >
-                  <h4>{options.name}</h4>
+                  <h4 className="flex items-center whitespace-nowrap	">
+                    {IconRenderOp && (
+                      <span className="text-[16px] mr-[5px]">
+                        <IconRenderOp />
+                      </span>
+                    )}
+                    {options.name}
+                  </h4>
                 </li>
               );
             })}
