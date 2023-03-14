c8d0eb3d51277a3042d5225edfbecc2b56482b22
unknown <angeleesc2@gmail.com>
2023-03-14 12:51:18 -0400

    clasificacion de la imagen

--- src/components/form-controls/drop-zone/MultimediaZone.jsx
+++ src/components/form-controls/drop-zone/MultimediaZone.jsx
@@ -4,8 +4,12 @@ import { isVideoFileRegEx } from "../../../helpers/regEx";
 export default function MultimediaZone({ value }) {
   const { type } = value[0];
   console.log("el tipo recibido es", type);
+  const typeV = /video/;
+  const typeI = /image/;
 
-  if (type==="video/mp4") return <div>es un video</div>;
-
+  // if (type==="video/mp4") return <div>es un video</div>;
+  // if(ind)
+  if (type.indexOf("image") >= 0) return <h3>es una imagen</h3>;
+  if (type.indexOf("video") >= 0) return <h3>es un video</h3>;
   return <h3>desconocido</h3>;
 }
