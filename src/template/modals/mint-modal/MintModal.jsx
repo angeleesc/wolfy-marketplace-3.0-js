106ca6a24439130ce76fa683c1e93b7a71f87c88
unknown <angeleesc2@gmail.com>
2023-03-17 12:54:15 -0400

    racion de las imagenes

--- src/template/modals/mint-modal/MintModal.jsx
+++ src/template/modals/mint-modal/MintModal.jsx
@@ -1,40 +1,26 @@
 import React, { useState } from "react";
 import WolfyModalLayoutReduxController from "../../../components/layout/WolfyModalLayoutReduxController";
 import { keyModalSate } from "../../../features/modals/modalsSlice";
-import { useSelector } from "react-redux";
 import "./mint-modal.scss";
 import { useWFileContex } from "../../../context/FileContex";
+import MultimediaZone from "./MultimediaZone";
 
-const cuadradopromise = (value) => {
-  return new Promise((resolve, rejet) => {
-    setTimeout(() => {
-      resolve({
-        value,
-        result: value * value,
-      });
-    }, 1000);
-  });
-};
 export default function MintModal() {
   const [stepProcess, setStepProcess] = useState(0);
   const [ethereumStepProcess, setEthereumStepProcess] = useState(5);
   const fileContext = useWFileContex();
 
+  const { metadataFile } = fileContext.curentfile;
+
   console.log("el modal recibio");
   console.log(fileContext.curentfile);
   console.log("fin del recibimiento");
-
-  const modalData = useSelector(
-    (state) => state.modals[keyModalSate.mintModal].dataToProccess
-  );
-
-  // console.log(modalData);
 
   return (
     <WolfyModalLayoutReduxController modalController={keyModalSate.mintModal}>
       <div className="wolf-modal-body w-[100%] max-w-[560px] min-h-[40px]">
         <div className="header">
-          
+          <MultimediaZone file={metadataFile} />
         </div>
       </div>
     </WolfyModalLayoutReduxController>
--- src/template/modals/mint-modal/MultimediaZone.jsx
+++ src/template/modals/mint-modal/MultimediaZone.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
 
-export default function MultimediaZone() {
-  return (
-    <div className='mint-prewie-file'>
-        
-    </div>
-  )
+export default function MultimediaZone({ file }) {
+  const { type } = file[0];
+  const url = URL.createObjectURL(file[0]);
+
+  if (type.indexOf("image") >= 0) {
+    const url = URL.createObjectURL(file[0]);
+    return (
+      <div className="preview-box-container">
+        <img src={url} alt="prewie-image" />
+      </div>
+    );
+  }
+  if (type.indexOf("video") >= 0) {
+    return (
+      <div className="preview-box-container">
+        <video loop autoPlay muted>
+          <source src={url} type={type}></source>
+        </video>
+      </div>
+    );
+  }
+  return <h3>desconocido</h3>;
 }
