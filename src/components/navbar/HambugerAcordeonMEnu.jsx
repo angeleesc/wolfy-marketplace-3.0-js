fa8afc077530cf19c0c22757427abc556b20bd9e
unknown <angeleesc2@gmail.com>
2023-05-05 15:32:58 -0400

    configuracion de click pad animation

--- src/components/navbar/HambugerAcordeonMEnu.jsx
+++ src/components/navbar/HambugerAcordeonMEnu.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaPlus, FaMinus } from "react-icons/fa";
 
 export default function HambugerAcordeonMEnu({ title }) {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="wolfy-hambuger-acordeon-menu-container">
-      <button className="acordeon-menu-btn">{title}</button>
+      <button className="acordeon-menu-btn" onClick={() => setIsOpen(!isOpen)}>
+        <div className="acordeon-menu-btn-title">
+          <span>{title}</span>
+        </div>
+        <div className="acordeon-menu-chevron">
+          {isOpen ? <FaMinus /> : <FaPlus />}
+        </div>
+      </button>
     </div>
   );
 }
