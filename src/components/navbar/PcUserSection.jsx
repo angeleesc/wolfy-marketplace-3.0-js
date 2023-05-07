ecb68b1b787a0ae8a98c96c65a35677a37cdec25
unknown <angeleesc2@gmail.com>
2023-05-07 12:17:33 -0400

    seccion de usuario

--- src/components/navbar/PcUserSection.jsx
+++ src/components/navbar/PcUserSection.jsx
@@ -3,47 +3,52 @@ import WolfIdentidcon from "../identicon/WolfIdentidcon";
 import { Link } from "react-router-dom";
 import { staticRoutes } from "../../helpers/static-routes";
 import { FaRegUserCircle } from "react-icons/fa";
+import { useState } from "react";
 
 export default function PcUserSection({ valletAccount, balace }) {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="wof-pc-drop-container ml-1 mr-[15px]">
       <button className="avatar-zone">
         <WolfIdentidcon size={50} name={valletAccount} />
       </button>
-      <div className="wolf-pc-drop-box">
-        <div className="user-secttion-header">
-          <div className="avatar-section">
-            <WolfIdentidcon size={60} name={valletAccount} />
-          </div>
-          <div className="info-section">
-            <h4>Anonimous</h4>
-            <h6>
-              {valletAccount.substr(0, 10)}...{valletAccount.substr(-8)}
-            </h6>
-          </div>
-        </div>
-        {balace && (
-          <div className="token-secction">
-            <h3>Balance</h3>
-            <div className="token-box">
-              <div className="flex justify-between items-center">
-                <span className="symbol">ETH</span>
-                <span className="value">{Number(balace).toFixed(4)}</span>
-              </div>
+      {isOpen && (
+        <div className="wolf-pc-drop-box">
+          <div className="user-secttion-header">
+            <div className="avatar-section">
+              <WolfIdentidcon size={60} name={valletAccount} />
+            </div>
+            <div className="info-section">
+              <h4>Anonimous</h4>
+              <h6>
+                {valletAccount.substr(0, 10)}...{valletAccount.substr(-8)}
+              </h6>
             </div>
           </div>
-        )}
+          {balace && (
+            <div className="token-secction">
+              <h3>Balance</h3>
+              <div className="token-box">
+                <div className="flex justify-between items-center">
+                  <span className="symbol">ETH</span>
+                  <span className="value">{Number(balace).toFixed(4)}</span>
+                </div>
+              </div>
+            </div>
+          )}
 
-        <div className="p-[10px]">
-          <Link
-            to={staticRoutes.userData(valletAccount)}
-            className="flex items-center"
-          >
-            <FaRegUserCircle className="mr-2 text-wolf-gray-light-2200 text-[24px]" />{" "}
-            <h3>Perfil</h3>
-          </Link>
+          <div className="p-[10px]">
+            <Link
+              to={staticRoutes.userData(valletAccount)}
+              className="flex items-center"
+            >
+              <FaRegUserCircle className="mr-2 text-wolf-gray-light-2200 text-[24px]" />{" "}
+              <h3>Perfil</h3>
+            </Link>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
