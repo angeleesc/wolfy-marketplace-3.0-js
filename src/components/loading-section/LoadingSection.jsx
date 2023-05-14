9e93f702ee2a0536d8812b7682cb3abeb5db4613
unknown <angeleesc2@gmail.com>
2023-05-14 15:44:09 -0400

    creacion del paner de cargando 1

--- src/components/loading-section/LoadingSection.jsx
+++ src/components/loading-section/LoadingSection.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import WolfTinking from "../icons/WolfTinking";
+import "./loading-secction.scss";
 
 export default function LoadingSection() {
   return (
     <div className="loading-secction">
       <h3>Cargando</h3>
       <div className="icon section">
-        <WolfTinking  size="90" />
+        <WolfTinking size="90" />
       </div>
+      <span>estamos obteniendo todos los datos acerca de la nft</span>
     </div>
   );
 }
