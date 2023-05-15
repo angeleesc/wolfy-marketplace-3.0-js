0887538ea04b37dba358d44213820f2c3d596ef6
unknown <angeleesc2@gmail.com>
2023-05-15 12:25:47 -0400

    confuguracion de la pantallla de carga

--- src/components/loading-section/LoadingSection.jsx
+++ src/components/loading-section/LoadingSection.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import WolfTinking from "../icons/WolfTinking";
 import "./loading-secction.scss";
+import { Skeleton } from "@mui/material";
+
 
 export default function LoadingSection() {
   return (
@@ -8,6 +10,7 @@ export default function LoadingSection() {
       <h3>Cargando</h3>
       <div className="icon section">
         <WolfTinking size="90" />
+        <Skeleton animation="wave" width={"100%"} height={40} />
       </div>
       <span>estamos obteniendo todos los datos acerca de la nft</span>
     </div>
