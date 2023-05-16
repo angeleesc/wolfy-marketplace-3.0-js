ed729c87b0afaa2b33dfdb11e6924d99af8d557b
unknown <angeleesc2@gmail.com>
2023-05-16 16:26:29 -0400

    juetify

--- src/components/timer/Timer.jsx
+++ src/components/timer/Timer.jsx
@@ -4,8 +4,21 @@ import "./timer.scss";
 export default function Timer({ timeRef }) {
   return (
     <div className="timer-container">
-      <div>
-        <h3>hola</h3>
+      <div className="timer-item">
+        <span>Dias</span>
+        <h3>360</h3>
+      </div>
+      <div className="timer-item">
+        <span>Horas</span>
+        <h3>24</h3>
+      </div>
+      <div className="timer-item">
+        <span>Min</span>
+        <h3>60</h3>
+      </div>
+      <div className="timer-item">
+        <span>Seg</span>
+        <h3>60</h3>
       </div>
     </div>
   );
