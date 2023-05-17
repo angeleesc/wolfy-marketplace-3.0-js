834ea03efd9bd1453e17fe1c1b7e01aa4d5a5a6b
unknown <angeleesc2@gmail.com>
2023-05-17 19:39:51 -0400

    configuracion de los datos

--- src/components/bid-historys/BidHistoriesDisplay.jsx
+++ src/components/bid-historys/BidHistoriesDisplay.jsx
@@ -2,8 +2,19 @@ import React from "react";
 import "./hisotry-display.scss"
 
 export default function BidHistoriesDisplay({ orderDb }) {
-  console.log("orden de subasta a consultar");
-  console.log(orderDb);
+//   console.log("orden de subasta a consultar");
+//   console.log(orderDb);
+const [loading, setLoading] = useState(true);
+
+const [bids, setBids] = useState([])
+
+const init = async()=>{
+
+    if(orderDb){
+        const enpoint = ""
+    }
+
+}
 
   return (
     <div className="bid-histories-display">
