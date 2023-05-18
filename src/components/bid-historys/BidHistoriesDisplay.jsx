b99a30f0133674fdae08543454c87dea2d0cf1f6
unknown <angeleesc2@gmail.com>
2023-05-17 20:49:48 -0400

    orden de las puja

--- src/components/bid-historys/BidHistoriesDisplay.jsx
+++ src/components/bid-historys/BidHistoriesDisplay.jsx
@@ -3,6 +3,8 @@ import "./hisotry-display.scss";
 import { requestEndPoints, rootApipaht } from "../../helpers/global-constants";
 import axios from "axios";
 import LoadingSection from "../loading-section/LoadingSection";
+import WolfDoubtful from "../icons/WolfDoubtful";
+import WolfIdentidcon from "../identicon/WolfIdentidcon";
 
 export default function BidHistoriesDisplay({ orderDb }) {
   //   console.log("orden de subasta a consultar");
@@ -20,7 +22,14 @@ export default function BidHistoriesDisplay({ orderDb }) {
       const result = await axios.get(endPont);
 
       console.log(result.data);
-      setLoading(false)
+
+      const { bidders, hasData, isSuccess } = result.data;
+
+      if (isSuccess && hasData) {
+        setBids(bidders);
+      }
+
+      setLoading(false);
     }
   };
 
@@ -35,12 +44,38 @@ export default function BidHistoriesDisplay({ orderDb }) {
           <LoadingSection />
         </div>
       ) : bids.length > 0 ? (
-        <div className="bisd-list">
-          <h3>Ofertas disponible</h3>
+        <div className="bisd-list-container">
+          <h3 className="font-semibold text-[20px]">Ofertas disponible</h3>
+          <span className="text-[14px] text-center font-semibold text-wolf-gray-light-1200 block w-[100%]">
+            Aqui estan los top 10 de mejores ofertas hecha para esta nft
+          </span>
+          {bids.map((bid, kei) => {
+            return (
+              <div key={orderDb + "-" + kei} className="bid-list-item">
+                <div className="bidder-section">
+                    <WolfIdentidcon size={30} name={bid.bidder} />
+                  <div className="ml-2" >
+                    <span>postor</span>
+                    <h4>{`${bid.bidder.substring(0, 5)}...${bid.bidder.substr(
+                      -5
+                    )}`}</h4>
+                  </div>
+                </div>
+                <div className="bid-sectio">
+                  <span>puja</span>
+                  <h4>{bid.bid}</h4>
+                </div>
+              </div>
+            );
+          })}
         </div>
       ) : (
-        <div className="not-bis-diplay">
+        <div className="flex flex-col items-center mt-[15px] p-[10px] bg-wolf-gray-dark-400 border border-wolf-gray-dark-800 rounded-[16px]">
           <h3>Sin ofertas</h3>
+          <WolfDoubtful />
+          <p className="text-[14px] text-center font-semibold text-wolf-gray-light-1200">
+            no hay ofertas asociadaqs a esta subasta
+          </p>
         </div>
       )}
     </div>
