24842d42b8a3faa64af60d50dc2685906e873f26
unknown <angeleesc2@gmail.com>
2023-05-19 15:07:20 -0400

    configuracion base del hero

--- src/pagers/Home.jsx
+++ src/pagers/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HeroSwiper from "../components/swipers/HeroSwiper";
+import Hero from "../template/pages/home/Hero";
 
 export default function Home() {
   return (
     <>
       {/* <HeroSwiper /> */}
+      <Hero/>
     </>
   );
 }
---
+++ src/template/pages/home/Hero.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./wolf-hero-v2.scss"
+
+
+export default function Hero() {
+  return (
+    <div className='wolf-hero-v2' >Hero</div>
+  )
+}
