4627eba09faa6375423af1c7b0de1a9f9e3e5373
unknown <angeleesc2@gmail.com>
2023-05-19 16:53:19 -0400

    confiiguracion de los caso

--- src/template/pages/home/Hero.jsx
+++ src/template/pages/home/Hero.jsx
@@ -1,15 +1,34 @@
-import React from 'react'
-import "./wolf-hero-v2.scss"
+import React from "react";
+import "./wolf-hero-v2.scss";
 
+// esta seccion esta dividido de dos formas
+// colleciones destacadas
+// en el tab sra de categoria
+// nfts destacadas
+// la nft con la oferta mas alta
+// la nft recien egregaga
+// la nft mas carra
+// la nft mas barata
 
 export default function Hero() {
   return (
-    <div className='wolf-hero-v2' >
+    <div className="wolf-hero-v2">
+      <section className="swiper-section">
+        <div className="hero-swiper-section-tab-menu">
+          <button className="hero-swiper-button-active">
+            <h3>Nfts</h3>
+          </button>
+          <button>
+            <h3>Arte</h3>
+          </button>
+          <button>
+            <h3>Gaming</h3>
+          </button>
+        </div>
 
-<section className='swiper-section'>
-<h3>swiper</h3>
-</section>
+        
 
+      </section>
     </div>
-  )
+  );
 }
