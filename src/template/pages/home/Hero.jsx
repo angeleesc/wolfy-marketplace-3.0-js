056ada734198d52c28050556de5d9f278131110c
unknown <angeleesc2@gmail.com>
2023-05-19 15:10:31 -0400

    configuracion de la secion de swiper

--- src/template/pages/home/Hero.jsx
+++ src/template/pages/home/Hero.jsx
@@ -4,6 +4,12 @@ import "./wolf-hero-v2.scss"
 
 export default function Hero() {
   return (
-    <div className='wolf-hero-v2' >Hero</div>
+    <div className='wolf-hero-v2' >
+
+<section className='swiper-section'>
+<h3>swiper</h3>
+</section>
+
+    </div>
   )
 }
