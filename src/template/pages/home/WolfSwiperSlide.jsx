df05d92659fb0f0540f2c563e3e72b7edc777e93
unknown <angeleesc2@gmail.com>
2023-05-21 10:07:31 -0400

    tab del inicio

--- src/template/pages/home/WolfSwiperSlide.jsx
+++ src/template/pages/home/WolfSwiperSlide.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 import { staticRoutes } from "../../../helpers/static-routes";
 import WolfIdentidcon from "../../../components/identicon/WolfIdentidcon";
 import { saleMethod } from "../../../helpers/global-constants";
-import { BsFillTagFill } from "react-icons/bs"
-import { ImClock}from "react-icons/im"
+import { BsFillTagFill } from "react-icons/bs";
+import { ImClock } from "react-icons/im";
 
 export default function WolfSwiperSlide({ data }) {
   console.log("datos obtenidos del swipert");
@@ -21,34 +21,37 @@ export default function WolfSwiperSlide({ data }) {
         <div className="swipe-info-body">
           {/* collectionFace */}
           <div className="flex items-center">
-          <div className="siwpe-avatar-section-item">
-                  <Link
-                    className="w-[100%] h-[100%] block"
-                    to={staticRoutes.colectionInfoData(data.colection)}
-                  >
-                    {data.collectionFace ? (
-                      <img src={data.collectionFace} alt="collection-face-photo" />
-                    ) : (
-                      <WolfIdentidcon name={data.colection} size={60} />
-                    )}
-                  </Link>
-                </div>
-            {metadata.nftName && <h3>{metadata.nftName}</h3>}
-          </div>
-          <div className="swipe-sale-info" >
-
-          <span className="price-info">
-                {data.saleMethod === saleMethod.sales ? "Precio" : "Puja Minima"}
-              </span>
-              <h4 className="flex items-center">
-                {data.saleMethod === saleMethod.sales ? (
-                  <BsFillTagFill className="mr-2" />
+            <div className="siwpe-avatar-section-item">
+              <Link
+                className="w-[100%] h-[100%] block"
+                to={staticRoutes.colectionInfoData(data.colection)}
+              >
+                {data.collectionFace ? (
+                  <img src={data.collectionFace} alt="collection-face-photo" />
                 ) : (
-                  <ImClock className="mr-2" />
+                  <WolfIdentidcon name={data.colection} size={60} />
                 )}
-                {data.saleMethod === saleMethod.sales ? data.price : data.currentPrice} ETH
-              </h4>
-
+              </Link>
+            </div>
+            <div>
+              <span>collecion {data.colectionName} </span>
+              {metadata.nftName && <h3>{metadata.nftName}</h3>}</div>
+          </div>
+          <div className="swipe-sale-info">
+            <span className="price-info">
+              {data.saleMethod === saleMethod.sales ? "Precio" : "Puja Minima"}
+            </span>
+            <h4 className="flex items-center">
+              {data.saleMethod === saleMethod.sales ? (
+                <BsFillTagFill className="mr-2" />
+              ) : (
+                <ImClock className="mr-2" />
+              )}
+              {data.saleMethod === saleMethod.sales
+                ? data.price
+                : data.currentPrice}{" "}
+              ETH
+            </h4>
           </div>
         </div>
       </div>
