510414ed943749621b7aee6210dabe108a9f8c09
unknown <angeleesc2@gmail.com>
2023-05-21 09:02:33 -0400

    imagen

--- src/template/pages/home/WolfSwiperSlide.jsx
+++ src/template/pages/home/WolfSwiperSlide.jsx
@@ -4,10 +4,19 @@ export default function WolfSwiperSlide({ data }) {
   console.log("datos obtenidos del swipert")
   console.log(data)
   const { type, metadata } = data;
+  console.log(metadata)
 
   return <div className="swipe-box" >
     {
       metadata.image && <img className="swiper-image" src={metadata.image} alt="swiper-image" />
     }
+    {/* <div className="swiper-nft-info" >
+
+      <div className="swipe-info-body">
+        {metadata.nftName && <h3>{metadata.nftName}</h3> }
+      </div>
+
+
+    </div> */}
   </div>;
 }
