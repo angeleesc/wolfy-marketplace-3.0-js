e4d95a1cf730ec13c1e0aa77356ab6ae2a1b63d0
unknown <angeleesc2@gmail.com>
2023-05-21 10:00:03 -0400

    secion de wolf

--- src/template/pages/home/WolfSwiperSlide.jsx
+++ src/template/pages/home/WolfSwiperSlide.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { staticRoutes } from "../../../helpers/static-routes";
 import WolfIdentidcon from "../../../components/identicon/WolfIdentidcon";
+import { saleMethod } from "../../../helpers/global-constants";
+import { BsFillTagFill } from "react-icons/bs"
+import { ImClock}from "react-icons/im"
 
 export default function WolfSwiperSlide({ data }) {
   console.log("datos obtenidos del swipert");
@@ -32,7 +35,21 @@ export default function WolfSwiperSlide({ data }) {
                 </div>
             {metadata.nftName && <h3>{metadata.nftName}</h3>}
           </div>
-          <div className="swipe-sale-info" ></div>
+          <div className="swipe-sale-info" >
+
+          <span className="price-info">
+                {data.saleMethod === saleMethod.sales ? "Precio" : "Puja Minima"}
+              </span>
+              <h4 className="flex items-center">
+                {data.saleMethod === saleMethod.sales ? (
+                  <BsFillTagFill className="mr-2" />
+                ) : (
+                  <ImClock className="mr-2" />
+                )}
+                {data.saleMethod === saleMethod.sales ? data.price : data.currentPrice} ETH
+              </h4>
+
+          </div>
         </div>
       </div>
     </div>
