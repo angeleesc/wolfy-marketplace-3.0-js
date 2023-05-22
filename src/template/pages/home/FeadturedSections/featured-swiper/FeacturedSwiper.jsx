be5e801b9e69cf6ec5a82335883ca7193c7ce490
unknown <angeleesc2@gmail.com>
2023-05-22 12:51:03 -0400

    configuracion de caso

--- src/template/pages/home/FeadturedSections/featured-swiper/FeacturedSwiper.jsx
+++ src/template/pages/home/FeadturedSections/featured-swiper/FeacturedSwiper.jsx
@@ -1,14 +1,29 @@
 import React from "react";
 import "./feactures-swiper.scss";
 
-export default function FeacturedSwiper({ data, loading, title, query = {} }) {
-  const loadingPlace = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+import { SwiperSlide, Swiper } from "swiper/react";
 
-  const init = async ()=>{
-    
-  }
+export default function FeacturedSwiper({ data, loading, title, query = {} }) {
+  const loadingCard = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  const init = async () => {};
 
   return (
-    <section className="feactured-section">{title && <h3>{title}</h3>}</section>
+    <section className="feactured-section">
+      {title && <h3>{title}</h3>}
+
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={3}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        {loadingCard.map((key, i) => {
+          return <SwiperSlide key={title+"-"+i}>
+            Slide {key}
+          </SwiperSlide>;
+        })}
+      </Swiper>
+    </section>
   );
 }
