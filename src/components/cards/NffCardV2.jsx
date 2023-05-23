18ffe829e1f5beba685ebbeb3425165cac3268bb
unknown <angeleesc2@gmail.com>
2023-05-23 13:05:29 -0400

    configuracion de carta

--- src/components/cards/NffCardV2.jsx
+++ src/components/cards/NffCardV2.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "./nft-card-v2.scss"
+import { Skeleton } from "@mui/material";
 
 export default function NffCardV2({ data, loading }) {
   return loading ? (
     <div className="laoding-card">
-      <h3>cargando</h3>
+      <div className="img-skeletor" >
+        
+      </div>
     </div>
   ) : (
     <div></div>
