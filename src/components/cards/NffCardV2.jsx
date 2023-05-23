b9053f2af047f0a4d987039666a03d8ee4780bf8
unknown <angeleesc2@gmail.com>
2023-05-23 15:26:42 -0400

    swiper de destacados

--- src/components/cards/NffCardV2.jsx
+++ src/components/cards/NffCardV2.jsx
@@ -1,12 +1,31 @@
 import React from "react";
-import "./nft-card-v2.scss"
+import "./nft-card-v2.scss";
 import { Skeleton } from "@mui/material";
 
 export default function NffCardV2({ data, loading }) {
   return loading ? (
     <div className="laoding-card">
-      <div className="img-skeletor" >
-        
+      <div className="img-skeletor">
+        <Skeleton animation="wave" className="skeletor-text" sx={{
+          position:"absolute",
+          right: 15,
+          width: 80,
+          bottom: 10
+        }} />
+        <div className="info-skeletor">
+          <Skeleton
+            variant="circular"
+            width={50}
+            height={50}
+            animation="wave"
+            sx={{
+              position: "absolute",
+              bottom: -20,
+              left: 20
+            }}
+          />
+       
+        </div>
       </div>
     </div>
   ) : (
