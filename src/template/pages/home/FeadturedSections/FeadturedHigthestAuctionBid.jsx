d5a017e21a8bad97bbe5a42b8cec3fb561dd5d48
unknown <angeleesc2@gmail.com>
2023-05-24 13:40:42 -0400

    sor by option

--- src/template/pages/home/FeadturedSections/FeadturedHigthestAuctionBid.jsx
+++ src/template/pages/home/FeadturedSections/FeadturedHigthestAuctionBid.jsx
@@ -1,43 +1,54 @@
-import React from 'react'
-import FeacturedSwiper from './featured-swiper/FeacturedSwiper'
-import { useState } from 'react'
-import { requestEndPoints, rootApipaht, saleMethod } from '../../../../helpers/global-constants'
-import axios from 'axios'
-import { useEffect } from 'react'
+import React from "react";
+import FeacturedSwiper from "./featured-swiper/FeacturedSwiper";
+import { useState } from "react";
+import {
+  requestEndPoints,
+  rootApipaht,
+  saleMethod,
+} from "../../../../helpers/global-constants";
+import axios from "axios";
+import { useEffect } from "react";
 
 export default function FeadturedHigthestAuctionBid() {
+  const [laoding, setLaoding] = useState(true);
+  const [nftsData, setNftsData] = useState([]);
 
- const [laoding, setLaoding] = useState(true)
- const [nftsData, setNftsData] = useState([])
+  const init = async () => {
+    const endpoint =
+      rootApipaht.local + requestEndPoints.firebase.GETOrdersByQuery;
 
- const init = async()=>{
+    console.log(endpoint);
 
-  const endpoint = rootApipaht.local + requestEndPoints.firebase.GETOrdersByQuery
+    const query = [
+      // {
+      //   type: "where",
+      //   index: "saleMethod",
+      //   operator: "==",
+      //   value: saleMethod.auction,
+      //   valueType: "string"
+      // },
+      {
+        type: "where",
+        index: "endTime",
+        operator: ">=",
+        value: Date.now(),
+        valueType: "date"
+      }
+    ];
 
+    
+    const result = await axios.get(endpoint, {
+      params: {
+        query: JSON.stringify(query),
+      },
+    });
 
-  console.log(endpoint)
+    console.log(result.data);
+  };
 
-  const result = await axios.get(endpoint, {
-    params: {
-      saleMethod:saleMethod.auction,
-      endTime: Date.now(),
-      sortBy: "endTime"
+  useEffect(() => {
+    init();
+  }, []);
 
-
-    }
-  })
-
-  console.log(result.data)
-
- }
-
- useEffect(()=>{
-
-  init()
-
- }, [])
-
-  return (
-   <FeacturedSwiper title={"Subastas destacadas"}  loading={laoding} />
-  )
+  return <FeacturedSwiper title={"Subastas destacadas"} loading={laoding} />;
 }
